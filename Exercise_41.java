1077c9379eb1d8cdd7f0b7db29e532de0d89c258
soumava <soumavadas06@gmail.com>
2023-03-09 20:03:50 +0530

    Day-14

---
+++ Exercise_41.java
@@ -0,0 +1,7 @@
+// Write a Java program to print the ascii value of given character.
+public class Exercise_41 {
+    public static void main(String[] String) {
+        int chr = 'Z';
+        System.out.println("The ASCII value of Z is :"+chr);
+    }
+}

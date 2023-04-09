9b0145f8116418f51469e9af96a193dbb3b47d09
soumava <soumavadas06@gmail.com>
2023-04-09 19:50:18 +0530

    Day-43

---
+++ Exercise_108.java
@@ -0,0 +1,14 @@
+// Write a Java program to add all the digits of a given positive integer until the result has a single digit.
+import java.util.Scanner;
+public class Exercise_108 {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a positive integer: ");
+        int n = in.nextInt();
+        if (n>0)
+        {
+            System.out.print("The single digit number is: "+(n == 0 ? 0 : (n % 9 == 0 ? 9 : n % 9)));
+        }
+        System.out.println("\n");
+    }
+}

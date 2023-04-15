55eaa845fc43d74b60feac33ed7758f5a8242d59
Soumava Das <soumavadas06@gmail.com>
2023-04-15 06:27:55 +0000

    Day-48

--- Exercise_116.java
+++ Exercise_116.java
@@ -1,4 +1,5 @@
-// import java.util.*;
+// Write a Java program which iterates the integers from 1 to 100. For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". When number is divided by both three and five, print "fizz buzz".
+import java.util.*;
 public class Exercise_116 {
     public static void main(String[] args)
     {

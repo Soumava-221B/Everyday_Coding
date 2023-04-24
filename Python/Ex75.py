f029085658d425c6f57991ab385f2268a3065ca6
Soumava Das <soumavadas06@gmail.com>
2023-04-24 12:16:51 +0000

    Day-56

---
+++ Python/Ex75.py
@@ -0,0 +1,6 @@
+# Write a Pyhton program to get the copyright information and write Copyright information in Pyhton code.
+
+import sys
+print("\nPython Copyright Information")
+print(sys.copyright)
+print()

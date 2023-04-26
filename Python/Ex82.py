532e609545a59d504d62f56caa9c554071e05853
Soumava Das <soumavadas06@gmail.com>
2023-04-26 09:11:08 +0000

    Day-58

---
+++ Python/Ex82.py
@@ -0,0 +1,5 @@
+# Write a Pyhton program to calculate the sum of all items of a container (tuple, list, set, dictionary)
+
+s = sum([10, 20, 30])
+print("\nSum of the container: ", s)
+print()
---
+++ Python/Ex84.py
@@ -0,0 +1,7 @@
+# Write a Pyhton program to count the number of occurence of a specfic character in a string.
+
+s = "The quick brown fox jumps over the lazy dog."  
+print("Original string:")
+print(s)
+print("Number of occurrence of 'o' in the said string:")
+print(s.count("o"))

97de5bb2a544d0d90e0a3bd9152f0ec64dd8a05c
Soumava Das <soumavadas06@gmail.com>
2023-05-06 04:51:32 +0000

    Day-66

---
+++ Python/Ex114.py
@@ -0,0 +1,6 @@
+#Write  a Python to filter positive numbers from a list.
+
+nums = [34, 1, 0, -23, 12, -88]
+print("Original numbers in the list: ",nums)
+new_nums = list(filter(lambda x: x > 0, nums))
+print("Positive numbers in the said list: ", new_nums)

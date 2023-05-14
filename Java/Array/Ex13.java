4b450ed05167ad8e724008857a1d9ebdb6e24509
Soumava Das <soumavadas06@gmail.com>
2023-05-14 06:06:57 +0000

    Day-74

---
+++ Java/Array/Ex13.java
@@ -0,0 +1,19 @@
+// Write a Java program to find duplicate values in an array of string values.
+
+public class Ex13 {
+public static void main(String[] args) 
+    {
+        String[] my_array = {"bcd", "abd", "jude", "bcd", "oiu", "gzw", "oiu"};
+ 
+        for (int i = 0; i < my_array.length-1; i++)
+        {
+            for (int j = i+1; j < my_array.length; j++)
+            {
+                if( (my_array[i].equals(my_array[j])) && (i != j) )
+                {
+                    System.out.println("Duplicate Element is : "+my_array[j]);
+                }
+            }
+        }
+    }    
+}

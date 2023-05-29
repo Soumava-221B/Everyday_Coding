4e222ff4d4e51ff7a47a9583051943fe46a5f18f
Soumava Das <soumavadas06@gmail.com>
2023-05-29 07:17:17 +0000

    Strings

---
+++ Python/Strings/Ex11.py
@@ -0,0 +1,11 @@
+# Write a Pyhton program to remove characters that have odd index values in a given string 
+
+def odd_values_string(str):
+    result = ""
+    for i in range(len(str)):
+        if i % 2 == 0:
+            result = result + str[i]
+    return result
+
+print(odd_values_string('abcdef'))
+print(odd_values_string('python'))

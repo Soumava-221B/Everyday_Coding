085f23472746fb591269e38e0ca3d91d5ca7f2db
Soumava Das <soumavadas06@gmail.com>
2023-06-05 18:18:03 +0000

    Plus Minus Hacker rank question

---
+++ Java/plus_minus.java
@@ -0,0 +1,77 @@
+/*
+Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^ -4 are acceptable.
+
+Example :
+arr = [1,1,0, -1,-1]
+
+There are n =5 elements, two positive, two negative and one zero. Their ratios are 2/5=0.400000 , 2/5=0.400000  and 1/5 = 0.200000 . Results are printed as: 
+
+0.400000
+0.400000
+0.200000
+
+Function Description
+
+Complete the plusMinus function in the editor below.
+
+plusMinus has the following parameter(s):
+
+    int arr[n]: an array of integers
+
+Print
+Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with 6 digits after the decimal. The function should not return a value.
+
+Input Format
+
+The first line contains an integer n, the size of the array.
+The second line contains space-separated integers that describe arr[n]
+
+Constraints
+
+0 < n < 100
+-100 <= arr[i] <=100
+
+Output Format
+
+Print the following
+lines, each to
+
+decimals:
+
+1. proportion of positive values
+2. proportion of negative values
+3. proportion of zeros
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class plus_minus {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Eneter the number of elements in the array: ");
+        int n = in.nextInt();
+        int pos = 0;
+        int zero = 0;
+        int neg = 0;
+        System.out.println("Enter the number in the array: ");
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            if (x > 0) {
+                pos++;
+            } else if (x == 0) {
+                zero++;
+            } else {
+                neg++;
+            }
+        }
+        System.out.println(pos / (double) n);
+        System.out.println(neg / (double) n);
+        System.out.println(zero / (double) n);
+    }
+}

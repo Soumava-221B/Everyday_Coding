c1086219e56e417ddff41a0563442624e6477222
Soumava Das <soumavadas06@gmail.com>
2023-06-11 15:50:34 +0000

    Empty file check code

---
+++ Java/Exceptional Handling/Ex5.java
@@ -0,0 +1,38 @@
+// write a java program that reads a file and throws an exception if the file is empty. 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String args[]) {
+        try {
+            checkFileNotEmpty("test1.txt");
+            System.out.println("File is not empty.");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+        catch (EmptyFileException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public static void checkFileNotEmpty(String fileName) throws FileNotFoundException, EmptyFileException {
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+
+        if(!sc.hasNextLine()) {
+            sc.close();
+            throw new EmptyFileException("File is empty.");
+        }
+
+        sc.close();
+    }
+}
+
+class EmptyFileException extends Exception {
+    public EmptyFileException(String message) {
+        super(message);
+    }
+}

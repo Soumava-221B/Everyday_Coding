14fbe21f578fecf1457d2f90b905f8d2da4471d2
Soumava Das <soumavadas06@gmail.com>
2023-07-17 15:30:27 +0000

    Ex12 of Regular Expression

---
+++ Java/Regular Expression/Ex12.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Ex12 {
+ 
+   public static void main(String[] args) {
+	   
+	    String text = "Python is an interpreted high-level general-purpose programming language. " + 
+		"Its design philosophy emphasizes code readability with its use of significant indentation. " ;
+		System.out.println("Original string:");
+		System.out.println(text);
+		System.out.println("\nAfter replacing Python to Java and code to coding in the said string:");
+		System.out.println(validate(text));
+        }
+
+   public static String validate(String text) {
+	   return text.replace("Python", "Java").replace("code", "coding");
+   }
+}

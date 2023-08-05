723514330e99d5001e3f64adfdd58a4e035d656e
Soumava Das <soumavadas06@gmail.com>
2023-08-05 06:09:42 +0000

    Implementation sales by match

---
+++ HackerRank/Sales_by_match.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Sales_by_match {
+
+public static void main(String[] args) {
+
+Scanner sc = new Scanner(System.in);
+int[] colors = new int[101];
+int ans=0;
+int n = sc.nextInt();
+for(int i = 0;i<n;i++){
+int color=sc.nextInt();
+colors[color]++;
+if(colors[color]%2 == 0){
+ans++;
+colors[color] = 0;
+}
+}
+System.out.println(ans);
+}
+}

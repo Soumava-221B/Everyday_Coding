eb07f8a6f20c7387c4d8bf9c0d5090eafdcbdac4
Soumava Das <soumavadas06@gmail.com>
2023-08-15 15:58:16 +0000

    HackerRank Python coding challenge

---
+++ HackerRank/Python/leapyear.py
@@ -0,0 +1,16 @@
+def is_leap(year):
+    leap = False
+    
+    leap = False
+    if (year % 400 == 0) and (year % 100 == 0):
+        leap = True
+
+    elif (year % 4 ==0) and (year % 100 != 0):
+        leap = True
+
+    else:
+        pass
+    
+    return leap
+
+year = int(input())

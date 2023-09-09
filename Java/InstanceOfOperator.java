5c977a6190bec0146cbd04a455c48a5750544869
Soumava Das <soumavadas06@gmail.com>
2023-09-09 15:04:36 +0000

    Example of Instane of Operator

---
+++ Java/InstanceOfOperator.java
@@ -0,0 +1,9 @@
+public class InstanceOfOperator {
+
+    public static void main(String args[]) {
+
+        InstanceOfOperator obj = null;
+
+        System.out.println(obj instanceof InstanceOfOperator);
+    }
+}
